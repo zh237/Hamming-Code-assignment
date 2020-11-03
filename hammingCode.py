fa22b40635ba48549414bb9ea5aa2c88f2e67f7f
LemonPepperSeasoning <tish386@aucklanduni.ac.nz>
2020-11-03 18:29:27 +1300

    added two test cases as demostration.

--- hammingCode.py
+++ hammingCode.py
@@ -5,13 +5,16 @@ from functools import reduce
 """
 For error detection, redundant bits are added to the current information to create hamming-code.
 The efficency increase with larger data, but remeber, hamming code can only detect up to 2 error and up to 1 error can be found and fixed.
-(This is extended hamming-code(8,4) )
+( This is extended hamming-code(8,4) : Index 0 is used for detecting two bit errors )
 """
 class HammingCode:
     def __init__(self, message, forChecking):
+        
         if (forChecking):
+            # input parameter will be used to create hamming code
             self.binary = message
         else:
+            # input parameter will be checked.
             self.data = message
   
 
@@ -23,24 +26,26 @@ class HammingCode:
             self.binary = self.data
         
         if (isinstance(self.data, str)):
-            self.binary = "".join(format(i, 'b')
+            binary_in_string = "".join(format(i, 'b')
                                 for i in bytearray(self.data, "utf8"))
-            # each character will be represented using 7
+            templist = []
+            for i in range(0,len(binary_in_string)):
+                templist.append(int(binary_in_string[i]))
+            self.binary = templist
 
     def createCode(self):
+        self.toBinary()
+    
         # Block length : 2**r  where r >= 2
         message_len = len( self.binary )
         r = 1
         while (2**r < message_len + r + 1 ):
             r+=1
-        print (r)
          
         self.binary.insert(0,0)
         for i in range(1, message_len + r + 1 ):
             if ( math.log(i,2) == math.ceil(math.log(i,2)) ):
                 self.binary.insert(i,0)
-        print (self.binary)
-        print (len(self.binary))
         
         for i in range(0,r):
             x = 2**i
@@ -48,10 +53,14 @@ class HammingCode:
                 if ( ((j>>i)&1)==1  ):
                     if (x!=j):
                         self.binary[x] = self.binary[x]^self.binary[j]
-        print (self.binary)
-        print (len(self.binary))
+
+    def getCode(self):
+        return self.binary
 
     def findError(self):
         pos = (reduce (lambda x, y: x^y , [i for i, bit in enumerate(self.binary) if bit]))
         binaryRep = format(pos, 'b')
-        print ("num : {} , Binary : {} ".format(pos, binaryRep))
+        if (pos != 0):
+            print ("Error : index num : {} , Binary : {} ".format(pos, binaryRep))
+        else:
+            print ("No error was detected !")
--- main.py
+++ main.py
@@ -3,17 +3,45 @@ from hammingCode import HammingCode
 import numpy as np
 import math
 
-# array = np.random.randint(0,2,20)
-# code = HammingCode(array)
-# print(code.toBinary())
-# print(code.createCode())
-# print (code.findError())
+""" 
+This file is used to show case the use of hammingCode. 
+"""
 
-test_case = [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1]
-code = HammingCode(test_case,True)
-code.findError()
+def testHammingCode(message, index_to_create_error):
+    code = HammingCode(message, False) 
+    code.createCode()
 
-# testString = "zadwawfca"
-# cod2 = HammingCode(testString)
-# print(cod2.toBinary())
-# print(code.createCode())
+    # since we have just created the code, there should not be any errors
+    code.findError() 
+
+    testcode = code.getCode()
+    # create an error manually. 
+    indexToChange = index_to_create_error #you can change this, and output will also change.
+    if (testcode[indexToChange] == 1):
+      testcode[indexToChange] = 0
+    else:
+     testcode[indexToChange] = 1
+    
+    code = HammingCode(testcode,True)
+
+    # Should print the value of "indexToChange"
+    code.findError()
+
+
+###  Test case 1 :
+array = np.random.randint(0,2,20)
+testHammingCode(array,6)
+"""
+Should print :
+No error was detected !
+Error : index num : 6 , Binary : 110 
+"""
+
+
+###  Test case 2 :
+testHammingCode("Testing hamming code",20)
+"""
+Should print :
+No error was detected !
+Error : index num : 20 , Binary : 10100  
+"""
