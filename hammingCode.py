14f210c02a558da003c14d3dc58770b177521822
LemonPepperSeasoning <tish386@aucklanduni.ac.nz>
2020-11-03 14:58:59 +1300

    first commit

---
+++ hammingCode.py
@@ -0,0 +1,20 @@
+import numpy as np
+from functools import reduce
+
+class hammingCode :
+    def _init_(self, data):
+        self.data
+        self.code = createCode()
+    
+    def createCode(self):
+        if (self.data.len() // 2 == 0):
+            return 1
+        else:
+            return 0
+    
+    def checkSize(self):
+        return 1
+    
+    def findError(self):
+        return 1
+    
